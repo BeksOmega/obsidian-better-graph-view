153c43b836c94db5e18ee2c14af6bbc459879dbe
Beka Westberg <bekawestberg@gmail.com>
2020-11-21 12:46:55 -0800

    Add setup datatypes

---
+++ src/graph-builders/i_graphbuilder.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2020 Obsidian Better Graph View
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Interface for a graph builder, which is an object that builds
+ *     a graph that sigma.js will accept.
+ * @author bekawestberg@gmail.com (Beka Westberg)
+ */
+'use strict';
+
+
+import {Vault} from "obsidian";
+import {Node} from "../../sigma/src/classes/sigma.classes.node";
+import {Edge} from "../../sigma/src/classes/sigma.classes.edge";
+
+
+/**
+ * Interface for a graph builder, which is an object that builds a graph that
+ * sigma.js will accept.
+ * @interface
+ */
+function GraphBuilder() {}
+
+/**
+ * Generates a graph for the given vault.
+ * @param {Vault} vault The vault to use to generate the graph.
+ * @return {{nodes: !Array<Node>, edges: !Array<Edge>}} A valid graph which
+ *     sigma.js will accept.
+ */
+GraphBuilder.prototype.generateGraph = function (vault) {
+  return {
+    nodes: [],
+    edges: [],
+  }
+};
